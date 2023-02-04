e6900f48232bc80a3e48c3c65cd40974f99b4f3d
Justin Fuller <justindanielfuller@gmail.com>
2023-02-04 08:29:42 -0500

    Recursive, arrays, completely empty.

--- zeroes.go
+++ zeroes.go
@@ -15,7 +15,26 @@ func Zeroes(m map[string]interface{}) map[string]interface{} {
 			if v == false {
 				delete(m, key)
 			}
+		case map[string]interface{}:
+			Zeroes(v)
+			if len(v) == 0 {
+				delete(m, key)
+			}
+		case []map[string]interface{}:
+			for i := len(v) - 1; i >= 0; i-- {
+				Zeroes(v[i])
+				if len(v[i]) == 0 {
+					v = v[:i]
+				}
+			}
+			if len(v) == 0 {
+				delete(m, key)
+			}
 		}
+	}
+
+	if len(m) == 0 {
+		return nil
 	}
 
 	return m
--- zeroes_test.go
+++ zeroes_test.go
@@ -28,6 +28,101 @@ func TestZeroes(t *testing.T) {
 				"true_bool":        true,
 			},
 		},
+		{
+			name: "one-layer-deep",
+			input: map[string]interface{}{
+				"recursive": map[string]interface{}{
+					"empty_int":        0,
+					"non_empty_int":    1,
+					"empty_string":     "",
+					"non_empty_string": "foo",
+					"false_bool":       false,
+					"true_bool":        true,
+				},
+				"recursive_array": []map[string]interface{}{
+					{
+						"empty_int":        0,
+						"non_empty_int":    1,
+						"empty_string":     "",
+						"non_empty_string": "foo",
+						"false_bool":       false,
+						"true_bool":        true,
+					},
+				},
+				"empty_int":        0,
+				"non_empty_int":    1,
+				"empty_string":     "",
+				"non_empty_string": "foo",
+				"false_bool":       false,
+				"true_bool":        true,
+			},
+			output: map[string]interface{}{
+				"recursive": map[string]interface{}{
+					"non_empty_int":    1,
+					"non_empty_string": "foo",
+					"true_bool":        true,
+				},
+				"recursive_array": []map[string]interface{}{
+					{
+						"non_empty_int":    1,
+						"non_empty_string": "foo",
+						"true_bool":        true,
+					},
+				},
+				"non_empty_int":    1,
+				"non_empty_string": "foo",
+				"true_bool":        true,
+			},
+		},
+		{
+			name: "empty-recursive",
+			input: map[string]interface{}{
+				"recursive": map[string]interface{}{
+					"empty_int":    0,
+					"empty_string": "",
+					"false_bool":   false,
+				},
+				"recursive_array": []map[string]interface{}{
+					{
+						"empty_int":    0,
+						"empty_string": "",
+						"false_bool":   false,
+					},
+				},
+				"empty_int":        0,
+				"non_empty_int":    1,
+				"empty_string":     "",
+				"non_empty_string": "foo",
+				"false_bool":       false,
+				"true_bool":        true,
+			},
+			output: map[string]interface{}{
+				"non_empty_int":    1,
+				"non_empty_string": "foo",
+				"true_bool":        true,
+			},
+		},
+		{
+			name: "empty",
+			input: map[string]interface{}{
+				"recursive": map[string]interface{}{
+					"empty_int":    0,
+					"empty_string": "",
+					"false_bool":   false,
+				},
+				"recursive_array": []map[string]interface{}{
+					{
+						"empty_int":    0,
+						"empty_string": "",
+						"false_bool":   false,
+					},
+				},
+				"empty_int":    0,
+				"empty_string": "",
+				"false_bool":   false,
+			},
+			output: nil,
+		},
 	}
 
 	for _, test := range tests {
